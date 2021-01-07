4301855e3d2d8ce904357fe400abb4b303a59eed
Deniz Eren Evrendilek <devrendilek@gmail.com>
2021-01-07 00:29:38 -0800

    initial commit with nextjs boilerplate code

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+const Index = () => {
+  return (
+    <div>Hello World.</div>
+  );
+}
+
+export default Index;
---
+++ src/pages/register.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface registerProps {
+
+}
+
+const Register: React.FC<registerProps> = ({}) => {
+    return (
+        <div>Register</div>
+    );
+}
+
+export default Register;
